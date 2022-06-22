366565f083cf3c8ce176c80f4dcbc47d6081da55
chayanjana1 <107670948+chayanjana1@users.noreply.github.com>
2022-06-22 13:41:45 +0530

    Add files via upload

---
+++ javac.java
@@ -0,0 +1,8 @@
+class javac{
+	public static void main (String[]args){
+		System.out.println("NAME : Chayan Jana");
+System.out.println("Course: B.TECH");
+System.out.println("Stream : CSE");
+			
+}
+}
