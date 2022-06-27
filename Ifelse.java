f8f202b25d6793635b5473b9a17c0bf0c0518830
chayanjana1 <107670948+chayanjana1@users.noreply.github.com>
2022-06-27 12:54:26 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+class Ifelse {
+	public static void main(String[] args){
+int a = 5;
+int b = 3;
+if(a+b < 10){
+System.out.println("a + b is less then 10");
+}else{
+System.out.println("a + b is greater than 10");
+}
+}
+}
